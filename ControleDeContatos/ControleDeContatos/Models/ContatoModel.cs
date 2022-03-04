9fa45da8a4c550283710e4dbb75e09958a37db6c
smichelle-dev <dev.smichelle@gmail.com>
2022-03-04 15:56:59 -0300

    criação do frontend + banco de dados

---
+++ ControleDeContatos/ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+       public int id { get; set; }
+        public string nome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+
+    }
+}
