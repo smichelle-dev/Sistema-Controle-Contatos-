572a3752c1701670820469200913a5af79487d73
smichelle-dev <dev.smichelle@gmail.com>
2022-03-06 16:54:15 -0300

    realizado alteração layout

--- ControleDeContatos/ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/ControleDeContatos/Models/ContatoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace ControleDeContatos.Models
     public class ContatoModel
     {
        public int id { get; set; }
+        [Required(ErrorMessage ="Digite o nome do contato")]
         public string nome { get; set; }
+        [Required(ErrorMessage = "Digite o e-mail do contato")]
+        [EmailAddress(ErrorMessage ="O e-mail informado não é valido")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Digite o celular do contato")]
+        [Phone(ErrorMessage ="O celular informado não é válido")]
         public string Celular { get; set; }
 
     }
